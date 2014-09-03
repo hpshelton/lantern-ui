e3f73621ada5b3fe0f686781a9f01f4b9d11489b
hpshelton <parker.shelton@gmail.com>
2014-09-03 00:27:17 -0700

    Add tests for Google analytics service

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -56,14 +56,16 @@ describe('The service', function() {
     var service, httpBackend;
     var API_URL_PREFIX = 'app/api/version';
 
-    beforeEach(module(function ($provide) {
-      $provide.value('API_URL_PREFIX', API_URL_PREFIX);
-    }));
+    beforeEach(function() {
+      module(function ($provide) {
+        $provide.value('API_URL_PREFIX', API_URL_PREFIX);
+      });
 
-    beforeEach(inject(function ($injector, apiSrvc) {
-      httpBackend = $injector.get('$httpBackend');
-      service = apiSrvc;
-    }));
+      inject(function ($injector, apiSrvc) {
+        httpBackend = $injector.get('$httpBackend');
+        service = apiSrvc;
+      });
+    });
 
     it('defines a API service', function () {
       expect(service).not.toEqual(undefined);
@@ -88,5 +90,87 @@ describe('The service', function() {
 
       service.exception(data);
     });
-  });  
+
+  });
+    
+  describe('Google Analytics Service', function () {
+    var service;
+    var fakeModelSrvc;
+    var fakeWindow;
+    var GOOGLE_ANALYTICS_WEBPROP_ID = '1';
+    var GOOGLE_ANALYTICS_DISABLE_KEY = 'ga-disable-' + GOOGLE_ANALYTICS_WEBPROP_ID;
+
+    beforeEach(function() {
+      module(function ($provide) {
+        $provide.value('API_URL_PREFIX', API_URL_PREFIX);
+        $provide.value('GOOGLE_ANALYTICS_WEBPROP_ID', GOOGLE_ANALYTICS_WEBPROP_ID);
+        $provide.value('GOOGLE_ANALYTICS_DISABLE_KEY', GOOGLE_ANALYTICS_DISABLE_KEY);
+
+        // Create a mock model for the service's use
+        fakeModelSrvc = jasmine.createSpyObj('modelSrvc', ['model']);
+        $provide.value('modelSrvc', fakeModelSrvc);
+      
+        // Create a mock analytics function for the service's use
+        fakeWindow = jasmine.createSpyObj('$window', ['ga']);
+        $provide.value('$window', fakeWindow);
+      });
+
+      inject(function ($injector, gaMgr) {
+        service = gaMgr;
+      });
+    });
+    
+    it('defines a Google Analytics service', function () {
+      expect(service).not.toEqual(undefined);
+      expect(service).not.toEqual(null);
+      expect(typeof service.stopTracking).toEqual('function');
+      expect(typeof service.startTracking).toEqual('function');
+      expect(typeof service.trackPageView).toEqual('function');        
+    });
+
+    it('defaults to tracking disabled', function () {
+      // TODO: This likely needs a stronger guarantee that we never called
+      // startTracking() but the spy setup is not cooperating
+      expect(fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY]).toEqual(true);
+    });
+
+    it('can enable tracking', function () {
+      fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY] = false;
+      service.startTracking();
+      expect(fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY]).toEqual(false);
+
+      fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY] = true;
+      service.startTracking();
+      expect(fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY]).toEqual(false);
+    });
+
+    it('can disable tracking', function () {
+      fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY] = false;
+      service.stopTracking();
+      expect(fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY]).toEqual(true);
+
+      fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY] = true;
+      service.stopTracking();
+      expect(fakeWindow[GOOGLE_ANALYTICS_DISABLE_KEY]).toEqual(true);
+    });
+
+    it('can report pageviews', function () {
+      service.startTracking();
+
+      service.trackPageView('start');
+      expect(fakeWindow.ga).toHaveBeenCalledWith('set', 'page', '/');
+      expect(fakeWindow.ga).toHaveBeenCalledWith('send', 'pageview', { sessionControl: 'start' });
+      fakeWindow.ga.reset();
+
+      service.trackPageView();
+      expect(fakeWindow.ga).toHaveBeenCalledWith('set', 'page', '/');
+      expect(fakeWindow.ga).toHaveBeenCalledWith('send', 'pageview', undefined);
+      fakeWindow.ga.reset();
+
+      fakeModelSrvc.model.modal = 'settings';
+      service.trackPageView('end');
+      expect(fakeWindow.ga).toHaveBeenCalledWith('set', 'page', 'settings');
+      expect(fakeWindow.ga).toHaveBeenCalledWith('send', 'pageview', { sessionControl: 'end' });
+    });
+  });
 });
